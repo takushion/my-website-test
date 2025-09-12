889d9cbdb63e3499c17c567320b4880dabb4e537
sato_takuma <daifuku.takumi3@gmail.com>
2025-09-13 03:25:12 +0900

    feat:ひな形の完成

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <p>自己紹介</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h2>ページが見つからないよ</h2>
+      <p>お探しのページは存在しないか、移動した可能性があります。</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
